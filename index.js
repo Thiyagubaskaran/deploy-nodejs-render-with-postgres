a05061f6de4a82c3a3dee366205bdfc52f24731b
Thiyagubaskaran <111138853+Thiyagubaskaran@users.noreply.github.com>
2024-03-12 18:02:17 +0530

    improve

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+// Adding a new route
+app.get('/about', (req, res) => {
+    res.send('This is the about page');
+});
+
+// Adding error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
